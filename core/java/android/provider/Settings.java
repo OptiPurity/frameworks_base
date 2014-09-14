8439950b47a8b15764c3c1182dca7409f61c07eb
AndroidRul3z <ferrettidario@gmail.com>
2014-09-15 00:02:58 +0200

    Flip to Mute/Reject Call
    Credits to OmniRom

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3041,6 +3041,13 @@ public final class Settings {
         public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
 
         /**
+         * Should Flip to Silence be used
+         *
+         * @hide
+         */
+        public static final String FLIP_ACTION_KEY = "flip_action";
+
+        /**
          * Show when WiFi or data mobile is sending/receiving data
          * @hide
          */
