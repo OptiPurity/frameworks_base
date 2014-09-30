CyanogenMod/android_frameworks_base

idmap: reduce CREATING STRING CACHE verbosity
Change-Id: I7febe358b3b302427650edc1d00772fc9d2d547c

libs/androidfw/ResourceTypes.cpp
@@ -635,7 +635,7 @@ const uint16_t* ResStringPool::stringAt(size_t idx, size_t* u16len) const
                                 mHeader->stringCount*sizeof(char16_t**)));
 #else
                         // We do not want to be in this case when actually running Android.
-                        ALOGW("CREATING STRING CACHE OF %d bytes",
+                        ALOGD("CREATING STRING CACHE OF %d bytes",
                                 mHeader->stringCount*sizeof(char16_t**));
 #endif
                         mCache = (char16_t**)calloc(mHeader->stringCount, sizeof(char16_t**));

OptiPurity/frameworks_base

Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

libs/androidfw/ResourceTypes.cpp
@@ -635,7 +635,7 @@ const uint16_t* ResStringPool::stringAt(size_t idx, size_t* u16len) const
                                 mHeader->stringCount*sizeof(char16_t**)));
 #else
                         // We do not want to be in this case when actually running Android.
-                        ALOGW("CREATING STRING CACHE OF %d bytes",
+                        ALOGV("CREATING STRING CACHE OF %d bytes",
                                 mHeader->stringCount*sizeof(char16_t**));
 #endif
                         mCache = (char16_t**)calloc(mHeader->stringCount, sizeof(char16_t**));